65b94769d07418149b538131ba9d6ec74e56b339
Andreas Schulz <andi.schulz@me.com>
2018-04-29 03:15:25 +0200

    Style cleanups.

--- asabridge/static/remove_readinglist.js
+++ asabridge/static/remove_readinglist.js
@@ -16,11 +16,10 @@ var SE = Application('System Events')
 var SF = SE.processes.byName('Safari')
 
 if (!Safari.running()) {
-	Safari.activate()
-	delay(2)
-	
+    Safari.activate()
+    delay(2)
 } else {
-	Safari.documents.push(Safari.Document())
+    Safari.documents.push(Safari.Document())
 }
 
 SF.frontmost = true
@@ -43,26 +42,22 @@ var sideBar = splitterGroup[0]
 var readingList = splitterGroup.groups[0].scrollAreas[0].tables[0]
 var realItems = []
 for (var i = 0; i < readingList.rows.length; ++i) {
-	if (readingList.rows[i].uiElements[0].groups.length != 0) {
-	    realItems.push(readingList.rows[i].uiElements[0])
-	}
+    if (readingList.rows[i].uiElements[0].groups.length != 0) {
+        realItems.push(readingList.rows[i].uiElements[0])
+    }
 }
 
 for (var i = 0; i < realItems.length; ++i) {
-	var hostName = realItems[i].uiElements[0].uiElements[2].value()
-	var previewText = realItems[i].uiElements[0].uiElements[1].value()
-	if (hostName === argv[4] && previewText === argv[5]) {
-	    var app = Application.currentApplication()
-		app.includeStandardAdditions = true
+    var hostName = realItems[i].uiElements[0].uiElements[2].value()
+    var previewText = realItems[i].uiElements[0].uiElements[1].value()
+    if (hostName === argv[4] && previewText === argv[5]) {
+        var app = Application.currentApplication()
+        app.includeStandardAdditions = true
 
-		var x = realItems[0].position()[0] + 5
-		var y = realItems[0].position()[1] + 5
-		app.doShellScript(`/usr/local/bin/cliclick rc:${x},${y}`)
-		readingList.menus[0].menuItems[5].actions.byName("AXPress").perform()
-    } else {
-        console.log("Mismatch:")
-        console.log(`"${hostName}"`)
-        console.log(`"${previewText}"`)
+        var x = realItems[0].position()[0] + 5
+        var y = realItems[0].position()[1] + 5
+        app.doShellScript(`/usr/local/bin/cliclick rc:${x},${y}`)
+        readingList.menus[0].menuItems[5].actions.byName("AXPress").perform()
     }
 }
 
