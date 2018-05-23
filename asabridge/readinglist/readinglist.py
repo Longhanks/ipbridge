dbb6fe2972af2546417c3a68511c3eca497c4109
Andreas Schulz <andi.schulz@me.com>
2018-05-23 21:19:00 +0200

    Image cache: Fix hash creation.

--- asabridge/readinglist/readinglist.py
+++ asabridge/readinglist/readinglist.py
@@ -21,7 +21,7 @@ DELETED_KEY = 'readinglist:deleted'
 def get_cached_image(image_url):
     if image_url is None:
         return None
-    file_name = hashlib.sha512('image_url'.encode()).hexdigest()
+    file_name = hashlib.sha512(image_url.encode()).hexdigest()
     tmp_path = Path('/tmp') / 'asabridge' / 'imagecache'
     if not tmp_path.exists():
         tmp_path.mkdir(parents=True)
