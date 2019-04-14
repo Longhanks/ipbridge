8544d21d7e474a0de33c9aaffc768ed6f5740070
Andreas Schulz <andi.schulz@me.com>
2019-04-14 20:12:45 +0200

    iCloud Tabs: Don't return UUID of tabs.

--- ipbridge/icloudtabs/icloudtabs.py
+++ ipbridge/icloudtabs/icloudtabs.py
@@ -8,11 +8,18 @@ _xpc_helper_dir = _this_file.parent.absolute() / 'safari-icloud-tabs-fetcher'
 _xpc_helper = _xpc_helper_dir / 'safari-icloud-tabs-fetcher'
 
 
-def get_icloud_tabs() -> dict:
+def get_icloud_tabs() -> list:
     try:
         text = subprocess.check_output(
             str(_xpc_helper), universal_newlines=True
         )
     except subprocess.SubprocessError:
-        text = '{"Tabs":[]}'
-    return json.loads(text)
+        text = '[]'
+    devices = json.loads(text)
+    icloud_tabs = []
+    for device in devices:
+        device_dict = {'DeviceName': device['DeviceName'], 'Tabs': []}
+        for tab in device['Tabs']:
+            device_dict['Tabs'].append({'Title': tab['Title'], 'URL': tab['URL']})
+        icloud_tabs.append(device_dict)
+    return icloud_tabs
